5f6168fd78d190d039db1943077cac20b6b09021
NolramGit <nolrammado@gmail.com>
2022-06-14 12:33:57 -0500

    upload file

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
-int main(){
-    puts("\"Programming is like building a multilingual puzzle");
-    return 0;
+
+/**
+ * main - Entry point
+ *
+ * Description: 'Prints message whit puts function
+ *
+ * Return: Return 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
