97775cccfaf3e483f42998d0b3bbf89736334464
NolramGit <nolrammado@gmail.com>
2022-06-14 14:32:15 -0500

    upload file

---
+++ hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'Prints message whit printf function
+ *
+ * Return: Return 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
