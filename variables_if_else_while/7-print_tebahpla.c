eb8e56e77e2950f680c2cd0ea35df0cb420ce8dd
NolramGit <nolrammado@gmail.com>
2022-06-14 22:38:43 -0500

    upload file

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'Prints message whit puts function
+ *
+ * Return: Return 0
+ */
+int main(void)
+{
+	int ascii = 112;
+
+	for (; ascii >= 97; ascii--)
+	{
+		putchar(ascii);
+	}
+	putchar(10);
+	return (0);
+}
