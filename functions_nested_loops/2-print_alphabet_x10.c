1166a1868f9863f3125450e3108557a527f2a430
NolramGit <nolrammado@gmail.com>
2022-06-15 13:41:23 -0500

    upload file

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints the alphabet 10 times
+ *
+ * Description: 'Prints _putchar whit function'
+ *
+ * Return: Return 0
+ */
+
+void print_alphabet_x10(void)
+{
+	int n = 1;
+
+	for (; n <= 10; n++)
+	{
+		int ltr = 97;
+
+		for (; ltr <= 122; ltr++)
+			_putchar(ltr)
+
+		_putchar(10)
+	}
+
+
+	_putchar('\n');
+}
