5f8db64a997be5c026c1a7a00f8a11e2d54d498f
NolramGit <nolrammado@gmail.com>
2022-06-15 12:24:48 -0500

    upload file

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -8,14 +8,6 @@
  */
 int main(void)
 {
-	/*_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');*/
-	_putchar('r');
-	/*_putchar('\n');*/
+	_putchar(53);
 	return (0);
 }
