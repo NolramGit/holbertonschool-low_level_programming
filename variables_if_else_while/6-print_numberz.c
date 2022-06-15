74b5a780c90d0fed4750d0311ed27205455f1a58
NolramGit <nolrammado@gmail.com>
2022-06-14 22:30:29 -0500

    upload file

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'Prints message whit puts function
+ *
+ * Return: Return 0
+ */
+int main(void)
+{
+	int n = 48;
+
+	for (; n <= 57; n++)
+	{
+		putchar(n);
+	}
+	putchar(10);
+	return (0);
+}
