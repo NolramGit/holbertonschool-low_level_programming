87eea785bb4741fd3ad35bdb20c94e115a69a35b
NolramGit <nolrammado@gmail.com>
2022-06-14 22:18:14 -0500

    upload file

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'Prints message whit puts function
+ *
+ * Return: Return 0
+ */
+int main(void)
+{
+	int ascii = 97;
+
+	for (; ascii <= 122; ascii++)
+	{
+		if (ascci != 101 && ascii != 113)
+			putchar(ascii);
+	}
+	putchar(10);
+	return (0);
+}
