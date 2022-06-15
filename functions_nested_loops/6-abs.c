b0bfe3935034bb532d0d3118c76a8100b78f401f
NolramGit <nolrammado@gmail.com>
2022-06-15 16:57:13 -0500

    upload file

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -1,15 +1,13 @@
 #include "main.h"
-#include <stdlib.h>
-#include <stdio.h>
 /**
- * int_abs - Function entry point.
+ * _abs - Function entry point.
  * @c : letra char.
  * Description: 'prints the absolute value of an integer.'
  *
  * Return: Return 0
  */
 
-int int_abs(int c)
+int _abs(int c)
 {
 	return (abs(c));
 }
