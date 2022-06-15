c3bdab9970335436a112f10440a1bee0414ced43
NolramGit <nolrammado@gmail.com>
2022-06-15 15:45:43 -0500

    upload file

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: 'Prints _putchar whit function'
+ *
+ * Return: Return 0
+ */
+
+int _islower(int c)
+{
+	if (islower(c))
+		return (1);
+	else
+		return (0);
+}
