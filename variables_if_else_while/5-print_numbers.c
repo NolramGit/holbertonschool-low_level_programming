fcd9ef60ecc4bd229f111f65e78ecabaeed2f2fe
NolramGit <nolrammado@gmail.com>
2022-06-14 22:25:03 -0500

    upload file

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'Prints message whit puts function
+ *
+ * Return: Return 0
+ */
+int main(void)
+{
+	int n = 0;
+
+	for (; n <= 9; n++)
+	{
+		printf("%d",n);
+	}
+	printf("\n")
+	return (0);
+}
