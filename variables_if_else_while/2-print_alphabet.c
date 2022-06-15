9905824f724f149eba20d9acb54efd7c9071b69e
NolramGit <nolrammado@gmail.com>
2022-06-14 20:04:26 -0500

    upload file

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'Prints message whit puts function
+ *
+ * Return: Return 0
+ */
+int main(void)
+{
+	int ascii = 97;
+
+	while (ascii <= 122)
+	{
+		putchar(ascii);
+		ascii++;
+	}
+	return (0)
+}
