cbf8e6de4d2865d92f6b1eb925133fcb7e3e17b3
NolramGit <nolrammado@gmail.com>
2022-06-20 17:16:02 -0500

    upload file

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * print_numbers - Entry function.
+ * Description : Function that prints the numbers 0 to 9 except 2 and 4.
+ */
+
+void print_numbers(void)
+{
+	int number;
+
+	for (number = 0; number <= 9 ; number++)
+	{
+		if (number != 2 && number != 4)
+			_putchar('0' + number);
+	}
+	_putchar(10);
+}
+
