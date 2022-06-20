76403ae06decd770ee80384fdeb922626a0f5ff6
NolramGit <nolrammado@gmail.com>
2022-06-20 16:55:35 -0500

    upload file

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,22 @@
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * _isdigit - Entry function.
+ * Description : Function that checks if variable @c is a digit. 
+ * @c: variable, argument
+ * Return: Always 0.
+ */
+
+int _isdigit(int c)
+{
+	if (isdigit(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
+
