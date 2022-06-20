0990fb11c52b95ab522869f6763f003adb814825
NolramGit <nolrammado@gmail.com>
2022-06-20 17:55:04 -0500

    upload file

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -3,21 +3,23 @@
 
 /**
  * more_numbers - Entry function.
- * Description : Function that prints the numbers 0 to 14.
+ * Description : Function that prints the numbers 0 to 14 10 times.
  */
 
 void more_numbers(void)
 {
 	int number;
+	int times;
 
-	for (number = 0; number <= 14 ; number++)
+	for (times = 0;  times <= 10 ; times++)
 	{
-		_putchar('0' + number);
-		if (number == 14)
+		for (number = 0; number <= 14 ; number++)
 		{
-			_putchar(10);
+			_putchar('0' + number)
+			if (number == 14)
+				_putchar(10);
 		}
+		_putchar(10);
 	}
-	_putchar(10);
 }
 
