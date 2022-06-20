4092e21ac44fe8c92f3989cc5e49f954efec8a74
NolramGit <nolrammado@gmail.com>
2022-06-20 16:59:23 -0500

    upload file

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * mul - Entry function.
+ * Description : Function that multiplies two integers.
+ * @a: variable, argument.
+ * @b: varible, argument.
+ * Return: Always 0.
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
