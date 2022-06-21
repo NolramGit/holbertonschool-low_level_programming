f6203e29b085ad24e7742e60758831427a2ee6d3
NolramGit <nolrammado@gmail.com>
2022-06-20 23:33:19 -0500

    upload file

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - Entry function 
+ * Description: change value of a pointer
+ * Return: Always 0.
+ */
+
+void reset_to_98(int *n)
+{
+	int *pointer &n;
+	*p = 98;
+}
