ffda7abb45d62ec41f2f5e695b303deb833f80e3
NolramGit <nolrammado@gmail.com>
2022-06-21 13:52:33 -0500

    Upload file

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strlen - Entry function
+ *
+ * Description: Function that returns length of two a string.
+ *
+ * @s: pointer.
+ *
+ * Return: Always 0.
+ */
+
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
