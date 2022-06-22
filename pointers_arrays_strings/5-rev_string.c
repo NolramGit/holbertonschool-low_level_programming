b0cf276f7e54620e15ed95707d6f565ed94f143e
NolramGit <nolrammado@gmail.com>
2022-06-21 22:38:29 -0500

    Upload file

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+/**
+ * rev_string - Entry function
+ *
+ * Description: Function that reverses a string.
+ *
+ * @s: pointer string.
+ *
+ */
+
+void rev_string(char *s)
+{
+	strrev(s);
+}
