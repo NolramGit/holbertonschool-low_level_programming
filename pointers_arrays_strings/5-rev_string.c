6f97fa22c67b8939cfc7c44560087f12bbd45446
NolramGit <nolrammado@gmail.com>
2022-07-08 17:36:14 -0500

    File upload

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -11,5 +11,21 @@
 
 void rev_string(char *s)
 {
-	strrev(s);
+	char str[500];
+	int i, j;
+
+	i = 0;
+	while (*(s + i) != 0)
+	{
+		str[i] = *(s + i);
+		i++;
+	}
+	j = i - 1;
+	i = 0;
+	while (j >= 0)
+	{
+		*(s + r) = str[i];
+		j--;
+		i++;
+	}
 }
