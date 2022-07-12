9edb2b95ec6dd999b26b5ea0603b7632de986d1e
NolramGit <nolrammado@gmail.com>
2022-07-11 19:44:14 -0500

    file upload

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _memcpy - Entry function.
+ * Description: Funtion that locates a character in a string.
+ *
+ * @s: char pointer
+ * @c: char pointer
+ *
+ * Return: 0
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		else
+			s++;
+	}
+	if (*s == c)
+		return (s);
+	else
+		return ('\0');
+}
+
