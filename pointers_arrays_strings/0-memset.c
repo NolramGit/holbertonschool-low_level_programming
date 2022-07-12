10d65189ac2323d8330d915e6f88d3b0dc459d78
NolramGit <nolrammado@gmail.com>
2022-07-11 19:37:55 -0500

    file upload

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _memset - Entry function. 
+ * Description: Funtion that fills memory whit a constant byte.
+ *
+ * @s: char pointer
+ * @b: char pointer
+ * @n: unsigned integer
+ *
+ * Return: pointer s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	i = 0;
+	while (i < n)
+	{
+		s[i] = b;
+		i++;
+	}
+	return (s);
+}
